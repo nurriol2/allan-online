b8ed85bf376ade051f3611ba43afeccb57407563
nurriol2 <nurriol2@illinois.edu>
2021-10-06 00:24:59 -0500

    Print computed coeffs on plot with absolute position

--- plotting.py
+++ plotting.py
@@ -1,4 +1,4 @@
-from coefficient_fitting import fit_rate_random_walk_line, fit_random_walk_line, fit_bias_instability_line
+from coefficient_fitting import fit_rate_random_walk_line, fit_random_walk_line, fit_bias_instability_line, find_index_of_closest_slope
 from plotly import graph_objects as go
 import plotly.express as px
 import numpy as np
@@ -33,14 +33,19 @@ def plot_allan_deviation(avg_time, allan_dev, noise_model, verbose):
                     labels=allan_deviation_labels)
     
     if verbose:
-        if noise_model[0]:
+        if noise_model:
             rw_line = fit_random_walk_line(avg_time, allan_dev)
-            fig.add_trace(go.Scatter(x=avg_time, y=rw_line, name="Random Walk", line=dict(dash="dash")))
+            fig.add_trace(go.Scatter(x=avg_time, y=rw_line[0], name=f"Random Walk", line=dict(dash="dash")))
+            fig.add_annotation(xref="paper", yref="paper", x=1, y=0.2, text=f"Calculated Random Walk Coefficient: {rw_line[1]:.3}...", showarrow=False)
+
         if noise_model[3]:
             rrw_line = fit_rate_random_walk_line(avg_time, allan_dev)
-            fig.add_trace(go.Scatter(x=avg_time, y=rrw_line, name="Rate Random Walk", line=dict(dash="dash")))
+            fig.add_trace(go.Scatter(x=avg_time, y=rrw_line[0], name="Rate Random Walk", line=dict(dash="dash")))
+            fig.add_annotation(xref="paper", yref="paper", x=1, y=0.1, text=f"Calculated Rate Random Walk Coefficient: {rrw_line[1]:.3}...", showarrow=False)
+
         if (noise_model[1] or noise_model[2]):
             bi_line = fit_bias_instability_line(avg_time, allan_dev)
-            fig.add_trace(go.Scatter(x=avg_time, y=bi_line, name="Bias Instability", line=dict(dash="dash")))
+            fig.add_trace(go.Scatter(x=avg_time, y=bi_line[0], name="Bias Instability", line=dict(dash="dash")))
+            fig.add_annotation(xref="paper", yref="paper", x=1, y=0.0, text=f"Calculated Bias Instability Coefficient: {bi_line[1]:.3}...", showarrow=False)
 
     return fig
