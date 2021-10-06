a214a00ee968c7eb5a7425e3cd223bd5344d92a5
nurriol2 <nurriol2@illinois.edu>
2021-10-05 23:05:43 -0500

    Parse the noise model to determine which coefficient fit lines to plot

--- plotting.py
+++ plotting.py
@@ -1,3 +1,5 @@
+from coefficient_fitting import fit_rate_random_walk_line, fit_random_walk_line, fit_bias_instability_line
+from plotly import graph_objects as go
 import plotly.express as px
 import numpy as np
 
@@ -17,7 +19,7 @@ def plot_time_series(time, y):
 
     return fig
 
-def plot_allan_deviation(avg_time, allan_dev):
+def plot_allan_deviation(avg_time, allan_dev, noise_model):
 
     allan_deviation_labels = {"Averaging Time":"\u03C4 (sec)",
                                 "Allan Deviation":"\u03C3(\u03C4)"}
@@ -29,5 +31,15 @@ def plot_allan_deviation(avg_time, allan_dev):
                     log_x=True,
                     log_y=True,
                     labels=allan_deviation_labels)
+
+    if noise_model[0]:
+        rw_line = fit_random_walk_line(avg_time, allan_dev)
+        fig.add_trace(go.Scatter(x=avg_time, y=rw_line, name="Random Walk", line_shape="linear"))
+    if noise_model[3]:
+        rrw_line = fit_rate_random_walk_line(avg_time, allan_dev)
+        fig.add_trace(go.Scatter(x=avg_time, y=rrw_line, name="Rate Random Walk", line_shape="linear"))
+    if (noise_model[1] or noise_model[2]):
+        bi_line = fit_bias_instability_line(avg_time, allan_dev)
+        fig.add_trace(go.Scatter(x=avg_time, y=bi_line, name="Bias Instability", line_shape="linear"))
 
     return fig
